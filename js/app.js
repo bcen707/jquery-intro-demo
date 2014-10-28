be2019ab69176809bf85a5e6256ef19ba524d44b
bcen707 <czarsalad707@gmail.com>
2014-10-28 09:45:52 -0700

    added new functions in class

--- js/app.js
+++ js/app.js
@@ -4,3 +4,39 @@
 
 "use strict";
 
+//select all navigation links
+var allNavlinks = $('nav a');
+
+var allImgs = $('img');
+
+var allSections = $('section');
+
+function onReady() {
+	$('nav').slideUp();
+	$('nav').slideDown();
+	allSections.hide();
+//	allNavlinks.attr('target', '_blank');
+//	allNavlinks.addClass('awesomesauce');
+
+}
+
+allImgs.hover(function() {
+	console.log($(this));
+	$(this).addClass('awesomesauce');
+});
+
+allImgs.mouseout(function() {
+	$(this).removeClass('awesomesauce');
+});
+
+allNavlinks.click(function() {
+	//the cooler jQuery this
+	console.log();
+	
+	console.log(this);
+
+	allSections.hide();
+	$($(this).attr('href')).fadeToggle(1000);
+});
+
+$(onReady);
