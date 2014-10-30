2ff849d6f5789afdeee55be297174e01cf3d8df1
bcen707 <czarsalad707@gmail.com>
2014-10-30 10:19:52 -0700

    added lodash functions

--- js/app.js
+++ js/app.js
@@ -4,6 +4,103 @@
 
 "use strict";
 
+var tile = 'img/sec1.jpg';
+var info = 'some other data';
+
+var gameBoard = $('#game-board');
+
+var newTile = $(document.createElement('img'));
+
+newTile.attr('src', 'img/background.jpg');
+newTile.attr('alt', 'photo of nature');
+newTile.attr('width', '250px');
+
+newTile.data('assocTile', tile);
+newTile.data('tileInfo', info);
+
+gameBoard.append(newTile);
+
+$('#game-board img').click(function() {
+	// 'this' refers to DOM element that raised the event
+	var clickedImage = $(this);
+	var tileData1 = clickedImage.data('assocTile');
+	var tileData2 = clickedImage.data('tileInfo');
+
+	clickedImage.attr('src', tileData1);
+	clickedImage.attr('alt', tileData2);
+
+	console.log(tileData1);
+	console.log(tileData2);
+
+});
+
+//iterate over array, calling passed function for each element
+//also works with objects
+
+var array = ['a', 'b', 'c', 'd'];
+
+_.forEach(array, function(element, index) {
+
+ console.log(element);
+ console.log(index);
+
+});
+
+_.difference(array, ['c', 'd']);
+
+var unionArray = _.union(array, ['e','f']);
+
+_.intersection(array, ['b', 'c']);
+
+var numArray = [1,2,3,4,5,6,7,8,9,10];
+
+var evens = _.filter(numArray, function(num) {return 0 == num % 2});
+console.log(evens);
+
+var shuffledArray = _.shuffle(numArray);
+
+console.log(shuffledArray);
+console.log(numArray);
+console.log(array);
+console.log(unionArray);
+
+/*
+
+
+
+
+*/
+
+var startTime = _.now();
+console.log(startTime);
+
+var timer;
+
+timer = window.setInterval(onTimer, 1000);
+
+function onTimer() {
+	// compare _.now() to start time to get elapsed time
+	//Math.floor() rounds down to the nearest integer
+	var elapsedSeconds = Math.floor((_.now() - startTime) / 10);
+	console.log(elapsedSeconds);
+	$('#game-board h2').css("font-size", elapsedSeconds + 'px');
+}
+
+function stopTimer() {
+	window.clearInterval(timer);
+	$('game-board').empty();
+}
+
+$('.jumbotron').click(stopTimer);
+
+
+
+
+
+
+
+
+/*
 //select all navigation links
 var allNavlinks = $('nav a');
 
@@ -39,4 +136,4 @@ allNavlinks.click(function() {
 	$($(this).attr('href')).fadeToggle(1000);
 });
 
-$(onReady);
+$(onReady); */
